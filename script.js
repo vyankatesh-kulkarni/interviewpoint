758cdc06f8f05635aed5eb7c4c81dd45e5ab743c
vyankatesh-kulkarni <vyankatesh15797.vk@gmail.com>
2020-01-04 15:46:14 +0530

    this is version 1

---
+++ script.js
@@ -0,0 +1,8 @@
+//javascrit file
+function change() {
+	ban=document.getElementById('img1');
+    if (ban.src="img/banner1.jpg") {
+		ban.src= "img/banner2.jpg";     
+                                   }   
+                 }
+
